5d3de3261f19a14f0471ba28cbeba43de18da03d
lazaryan <lazaryan99@gmail.com>
2018-12-02 02:25:54 +0300

    add new card

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,3 +1,27 @@
 let newSelect = document.querySelector('#categoryNewCard');
+let addNewCard = document.querySelector('#sendNewCard');
 
 createSelect(options, newSelect);
+
+addNewCard.addEventListener('click', (e) => {
+    let category = document.querySelector('#categoryNewCard').value;
+    let title = document.querySelector('#cardNewTitle').value;
+    let description = document.querySelector('#cardNewDesk').value;
+
+    let xhr = new XMLHttpRequest();
+
+    let text = `category="${category}"&title="${title}"&description="${description}"`;
+
+    xhr.open('POST', 'add_post.php', true);
+    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xhr.send(text);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState != 4) return;
+
+        if (xhr.status != 200) {
+            console.warn(xhr.status + ': ' + xhr.statusText);
+        } else {
+        }
+    };
+});
