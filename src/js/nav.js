11f18195bb55ca129d3077075aed230957ae6902
lazaryan <lazaryan99@gmail.com>
2018-12-01 21:26:02 +0300

    create select for nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,7 @@
 let active_search = document.querySelector('#searchAction');
 let search_text = document.querySelector('#searchText');
 let signOut = document.querySelector('#signOut');
+let select = document.querySelector('#navSelect');
 
 signOut.addEventListener('click', (e) => {
     let xhr = new XMLHttpRequest();
@@ -19,3 +20,35 @@ signOut.addEventListener('click', (e) => {
         }
     }
 })
+
+function createSelect(arr) {
+    let data = JSON.parse(arr);
+
+    data.forEach((option) => {
+        select.appendChild(createOption(option));
+    })
+}
+
+function createOption (el) {
+    let option = document.createElement('option');
+    option.innerHTML = el;
+    option.value = el;
+
+    return option;
+}
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('POST', './categories.php', true);
+xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+xhr.send(null);
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) return;
+
+    if (xhr.status != 200) {
+        console.warn(xhr.status + ': ' + xhr.statusText);
+    } else {
+        createSelect(xhr.responseText);
+    }
+}
